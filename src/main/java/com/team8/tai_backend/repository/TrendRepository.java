bcc46a42b9c8da536206f2f8196b6d3b9366c487
U-hee <uhee0112@gmail.com>
2025-10-31 14:51:49 +0900

    fix: change full DB fetch to query records between 00:00 and 24:00

--- src/main/java/com/team8/tai_backend/repository/TrendRepository.java
+++ src/main/java/com/team8/tai_backend/repository/TrendRepository.java
@@ -3,6 +3,8 @@ package com.team8.tai_backend.repository;
 import com.team8.tai_backend.entity.Trend;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -13,4 +15,5 @@ import java.util.Optional;
 public interface TrendRepository extends JpaRepository<Trend, Long> {
 
     Optional<Trend> findById(Long id);
+    List<Trend> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
