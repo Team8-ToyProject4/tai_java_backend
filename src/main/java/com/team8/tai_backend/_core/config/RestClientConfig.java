e8f330f29cbd068b81fd7a597fd3734ae5eecab5
MiSa-JD <jeongs0427@gmail.com>
2025-11-05 14:05:05 +0900

    removed unused imports

--- src/main/java/com/team8/tai_backend/_core/config/RestClientConfig.java
+++ src/main/java/com/team8/tai_backend/_core/config/RestClientConfig.java
@@ -10,11 +10,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
-import java.net.http.HttpClient;
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
